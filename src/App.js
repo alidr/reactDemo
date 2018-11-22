5b21045c43ffd7c1056f624e8a8563909218b51e
alidr <alidrzml@163.com>
2018-11-22 15:13:44 +0800

    reactDemo

--- src/App.js
+++ src/App.js
@@ -1,27 +1,69 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component,Fragment } from 'react';
+//引入react为了解析jsx语法
 
+// import {Component} from 'react';
+// 等价于
+// import React from 'react';
+// const Component = React.Component;
+// 使用的是es6的解构赋值
+//class 类 =》使组件app继承了react的compont的方法，这样就构建了一个组件
+
+import './style.css'
+//return加上括号可以多行写html标签
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      inputValue:'ali',
+      list:['w','z','c']
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        <div>
+          <label htmlFor="input">输入：</label>
+          <input type="text" 
+          id='input'
+          value={this.state.inputValue}
+          onChange={this.handleInput.bind(this)}/>
+          <button onClick={this.handleLi.bind(this)}>添加</button>
+        </div>
+        <ul>
+          {
+            this.state.list.map((item,index)=>{
+              return (
+                <li key={index}
+                onClick={this.handleLiDel.bind(this,index)}
+                >{item}</li>
+              )
+            })
+          }
+        </ul>
+      </Fragment>
     );
+  }
+
+  handleInput(e){
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+  handleLi(){
+    if (this.state.inputValue) {
+      this.setState({
+        list: [...this.state.list, this.state.inputValue],
+        inputValue: ""
+      })
+    }
+  }
+  handleLiDel(index){
+    const list = [...this.state.list];
+    list.splice(index,1);
+    this.setState({
+      list:list
+    })
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+//index.js 整个程序的入口文件
+//jsx语法
 import React from 'react';
+//第三方模块
+//把react生成的组件挂载到某一个节点下并显示出来
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//react思想：all in js
+// import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//一个组件
+import App from './App';
+//PWA 
+//支持https协议的服务器
+//用来存储访问的页面以便现实时显示
+// import * as serviceWorker from './serviceWorker';
+
+//ReactDom.render 一个方法
+//<App/> jsx语法啊
+ReactDOM.render( < App / > , document.getElementById('root'));
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
